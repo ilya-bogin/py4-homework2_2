431fc9a9b0ddf1aaceb3f71debc45d626c540b12
ilya-bogin <il.bogin@yandex.ru>
2017-05-28 10:10:51 +0300

    Update json parser

--- create_shop_list.py
+++ create_shop_list.py
@@ -1,41 +1,12 @@
-
-def read_recipe_name(file):
-  return file.readline().rstrip('\n').lower()
-
-
-def read_ingridients_count(file):
-  return int(file.readline())
-
-
-def read_ingridients(file, ingridients_count):
-  ingridients = []
-  for i in range(ingridients_count):
-    splitted_data = file.readline().rstrip('\n').split(" | ")
-
-    if len(splitted_data) != 3:
-      print('Wrong ingridients format')
-      return
-
-    ingridient = {
-      'ingridient_name' : splitted_data[0],
-      'quantity' : int(splitted_data[1]),
-      'measure' : splitted_data[2],
-    }
-    ingridients.append(ingridient)
-  return ingridients
-
+import json
 
 def read_all_recipes_from_file(file_name):
   recipes = {}
-  with open(file_name, newline='') as file:
-    while True:
-      recipe_name = read_recipe_name(file)
-      if len(recipe_name) == 0:
-        break;
-      ingridients_count = read_ingridients_count(file)
-      ingridients = read_ingridients(file, ingridients_count)
-      recipes[recipe_name] = ingridients
-  return recipes
+  with open(file_name) as json_data:
+    data = json.load(json_data)
+    for recipe in data:
+      recipes[recipe['recipe_name'].lower()] = recipe
+    return recipes
 
 
 def get_shop_list_by_dishes(cook_book, dishes, person_count):
@@ -44,7 +15,7 @@ def get_shop_list_by_dishes(cook_book, dishes, person_count):
     if dish not in cook_book:
       print('Блюдо {} не найдено'.format(dish))
       continue
-    for ingridient in cook_book[dish]:
+    for ingridient in cook_book[dish]['ingridients']:
       new_shop_list_item = dict(ingridient)
 
       new_shop_list_item['quantity'] *= person_count
@@ -60,7 +31,8 @@ def print_shop_list(shop_list):
                             shop_list_item['measure']))
 
 def create_shop_list():
-  recipes = read_all_recipes_from_file('recipes.txt')
+  recipes = read_all_recipes_from_file('recipes.json')
+  print(recipes)
   print('Доступные блюда:')
   for recipe_name in recipes.keys():
     print(recipe_name)
